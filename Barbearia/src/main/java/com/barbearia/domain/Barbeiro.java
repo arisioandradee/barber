a310a1861e0db81f140c0f5f93e248da1d413bc0
Arisio Andrade <arisiosaf@gmail.com>
2025-09-17 17:50:07 -0300

    Adding new files

--- Barbearia/src/main/java/com/barbearia/domain/Barbeiro.java
+++ Barbearia/src/main/java/com/barbearia/domain/Barbeiro.java
@@ -1,6 +1,5 @@
 package com.barbearia.domain;
 
-import com.barbearia.dto.BarbeiroDTO;
 import com.barbearia.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -9,14 +8,6 @@ import jakarta.persistence.Entity;
 public class Barbeiro extends Usuario{
 
 	private static final long serialVersionUID = 1L;
-	
-	public Barbeiro(BarbeiroDTO dto) {
-	    this.setId(dto.getId());
-	    this.setNome(dto.getNome());
-	    this.setCpf(dto.getCpf());
-	    this.setEmail(dto.getEmail());
-	    addPerfil(Perfil.CLIENTE);
-	}
 	
 	public Barbeiro() {
 		super();
